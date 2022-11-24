d023ccb9f31183c62a40fef9b7186d16cf1a0c33
Sag'indiq Begaydarov <sogindiq77@gmail.com>
2022-11-24 12:17:19 +0500

    changed port

--- dist/src/server.js
+++ dist/src/server.js
@@ -10,11 +10,12 @@ const error_middleware_1 = __importDefault(require("./middlewares/error.middlewa
 const routes_1 = __importDefault(require("./routes/routes"));
 const mongoose_1 = __importDefault(require("./utils/mongoose"));
 const app = (0, express_1.default)();
+// const PORT: string | number = process.env.PORT
+const PORT = process.env.PORT || 9000;
 app.use(express_1.default.json());
 app.use(routes_1.default);
 app.use(error_middleware_1.default);
 (0, mongoose_1.default)().then(() => console.log('Connected')).catch(err => console.log(err.message));
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(PORT);
 });
