52e4e9e4d42c0ac49790bfc2f06cc1be758033b5
Sag'indiq Begaydarov <sogindiq77@gmail.com>
2022-11-24 12:15:49 +0500

    changed port settings

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import errorHandle from './middlewares/error.middleware'
 import router from './routes/routes'
 import mongo from './utils/mongoose'
 const app: Application = express()
+// const PORT: string | number = process.env.PORT
+const PORT = process.env.PORT || 9000
 
 app.use(express.json())
 
@@ -14,7 +16,6 @@ app.use(errorHandle)
 
 mongo().then(() => console.log('Connected')).catch(err => console.log(err.message))
 
-const PORT: string | number = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(PORT)
